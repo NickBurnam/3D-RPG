8865c1ff7fcff706c039ec6101cc34a67d0e4164
NickBurnam <nickburnam789@gmail.com>
2026-02-10 18:17:53 -0800

    Player Movement
    Make the player movement relative to the orientation of the horizontal pivot, this way, 'w' is always forward and so on.

--- Player/player.gd
+++ Player/player.gd
@@ -32,10 +32,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := get_movement_direction()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -52,6 +49,16 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event is InputEventMouseMotion:
 			_look += -event.relative * mouse_sensitivity
 
+
+func get_movement_direction() -> Vector3:
+	# Get the player movement vector
+	var input_dir:Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	
+	# Normalize the X and Y input components and make a Vector3
+	var input_vector:Vector3 = Vector3(input_dir.x, 0, input_dir.y).normalized()
+	
+	# Multiply the basis of the horizontal pivot to our input vector
+	return horizontal_pivot.global_transform.basis * input_vector
 
 func frame_camera_rotation() -> void:
 	# Rotate about the Y axis (vertical) -> look left and right
