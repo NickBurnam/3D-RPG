a1cf11f9dfddd311525c933562cc865489b3b8a9
NickBurnam <nickburnam789@gmail.com>
2026-02-11 18:15:49 -0800

    Add Rig with movement
    Add player rig and use interpolation and look_at() to face the direction of movement smoothly.
    Remove old capsule mesh.

--- Player/player.gd
+++ Player/player.gd
@@ -11,10 +11,12 @@ var _look:Vector2 = Vector2.ZERO
 @export var mouse_sensitivity:float = 0.00075
 @export var min_boundary:float = -60.0 #degrees
 @export var max_boundary:float = 10.0 #degrees
+@export var animation_decay:float = 20.0
 
 @onready var smooth_camera_arm: SpringArm3D = $SmoothCameraArm
 @onready var horizontal_pivot: Node3D = $HorizontalPivot
 @onready var vertical_pivot: Node3D = $HorizontalPivot/VerticalPivot
+@onready var rig_pivot: Node3D = $RigPivot
 
 
 func _ready() -> void:
@@ -36,6 +38,9 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		# Update the Rig to face the direction of movement
+		look_toward_direction(direction, delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
@@ -75,3 +80,11 @@ func frame_camera_rotation() -> void:
 	
 	# Reset the _look vector for the next calculation
 	_look = Vector2.ZERO
+
+
+func look_toward_direction(direction:Vector3, delta:float) -> void:
+	# +Z axis is front, use looking_at() to calculate the new transform
+	var target_transform:Transform3D = rig_pivot.global_transform.looking_at(rig_pivot.global_position + direction, Vector3.UP, true)
+	
+	# Interpolate to the target transform using exp decay function: 1.0 - exp(-decay * delta)
+	rig_pivot.global_transform = rig_pivot.global_transform.interpolate_with(target_transform, 1.0 - exp(-animation_decay * delta))
